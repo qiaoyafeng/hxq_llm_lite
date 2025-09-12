22dc6b514acda101929aa96453f6e1fc395d83f1
qiaoyafeng <qiaoyafeng@gmail.com>
2025-09-12 17:41:37 +0800

    feat: update static js css

--- backend/hxq_llm_lite/main.py
+++ backend/hxq_llm_lite/main.py
@@ -3,6 +3,7 @@ from core.config import LOG_PATH, TEMP_PATH, settings
 from core.db import DATABASE_URL, check_db_connection
 from core.log import logger
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 from routers import chats
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
@@ -10,6 +11,8 @@ from starlette.staticfiles import StaticFiles
 app = FastAPI(
     title="好心情-LLM",
     summary="好心情-LLM",
+    docs_url=None,
+    redoc_url=None,
     version="1.0.0",
 )
 
@@ -48,6 +51,17 @@ logger.info(f"TEMP_PATH: {TEMP_PATH}")
 logger.info(f"LOG_PATH: {LOG_PATH}")
 logger.info(f"DATABASE_URL: {DATABASE_URL}")
 print(f"settings: {settings}")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/js/swagger-ui-bundle.js",
+        swagger_css_url="/static/js/swagger-ui.css",
+    )
 
 
 @app.get("/health")
